a40748c5c8ffb2561a6d66c40c9d2cb4c86e4ce9
Furqon August Seventeenth <furqonaugustseventeenth@gmail.com>
2026-06-23 14:33:05 +0700

    feat: add navigation onTap to home buttons
    Added GestureDetector and go_router integration to the Galeri, Peringkat, and Toko buttons on the HomeHeroSection to navigate to their respective routes.

--- lib/features/home/presentation/sections/home_hero_section.dart
+++ lib/features/home/presentation/sections/home_hero_section.dart
@@ -1,11 +1,15 @@
 import 'package:flutter/material.dart';
+import 'package:go_router/go_router.dart';
 
 import 'package:app/core/theme/app_colors.dart';
 import 'package:app/core/theme/app_spacing.dart';
 import 'package:app/core/theme/app_text_styles.dart';
+import 'package:app/features/home/presentation/widgets/home_start_button.dart';
+
 
 class HomeHeroSection extends StatelessWidget {
-  const HomeHeroSection({super.key});
+  final VoidCallback onStartPressed;
+  const HomeHeroSection({super.key, required this.onStartPressed});
 
   @override
   Widget build(BuildContext context) {
@@ -25,10 +29,9 @@ class HomeHeroSection extends StatelessWidget {
               fontWeight: FontWeight.w800,
               height: 1.15,
               letterSpacing: 0.32,
-            fontFamily: 'PoetsenOne'
+              fontFamily: 'PoetsenOne'
             ),
           ),
-          SizedBox(height: AppSpacing.lg),
           Text(
             'Ayo belajar menggambar hewan lucu dengan\ncara yang menyenangkan!',
             textAlign: TextAlign.center,
@@ -38,6 +41,67 @@ class HomeHeroSection extends StatelessWidget {
               height: 1.2,
             ),
           ),
+          HomeStartButton(onPressed: () => onStartPressed()),
+          SizedBox(height: 24),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+            children: [
+              GestureDetector(
+                onTap: () => context.push('/history'),
+                child: Container(
+                  height: 79,
+                  width: 106,
+                  decoration: BoxDecoration(
+                    color: Color(0xFFF3F3FA),
+                    borderRadius: BorderRadius.circular(16),
+                  ),
+                  child: Column(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      Icon(Icons.collections, color: Colors.blue, size: 30),
+                      Text('Galeri', style: AppTextStyles.bodyLarge.copyWith(fontWeight: FontWeight.bold, color: Colors.black)),
+                    ],
+                  )
+                ),
+              ),
+              GestureDetector(
+                onTap: () => context.push('/leaderboard'),
+                child: Container(
+                  height: 79,
+                  width: 106,
+                  decoration: BoxDecoration(
+                    color: Color(0xFFF3F3FA),
+                    borderRadius: BorderRadius.circular(16),
+                  ),
+                  child: Column(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      Icon(Icons.leaderboard, color: Color(0xFFFFD900), size: 30),
+                      Text('Peringkat', style: AppTextStyles.bodyLarge.copyWith(fontWeight: FontWeight.bold, color: Colors.black)),
+                    ],
+                  )
+                ),
+              ),
+              GestureDetector(
+                onTap: () => context.push('/shop'),
+                child: Container(
+                  height: 79,
+                  width: 106,
+                  decoration: BoxDecoration(
+                    color: Color(0xFFF3F3FA),
+                    borderRadius: BorderRadius.circular(16),
+                  ),
+                  child: Column(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      Icon(Icons.store, color: Color(0xFFF29D38), size: 30),
+                      Text('Toko', style: AppTextStyles.bodyLarge.copyWith(fontWeight: FontWeight.bold, color: Colors.black)),
+                    ],
+                  )
+                ),
+              ),
+            ],
+          )
         ],
       ),
     );

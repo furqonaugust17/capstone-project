f09f58460a712fba6b43bee8398114ce24630e62
Furqon August Seventeenth <furqonaugustseventeenth@gmail.com>
2026-07-29 02:13:58 +0700

    chore: update configuration for supabase

--- backend/prisma.config.ts
+++ backend/prisma.config.ts
@@ -4,6 +4,7 @@ import { defineConfig, env } from "prisma/config";
 export default defineConfig({
   schema: "prisma/schema.prisma",
   datasource: {
-    url: env("DATABASE_URL"),
+    url: env("DIRECT_URL"),
+    // shadowDatabaseUrl: env("DIRECT_URL")
   },
 });
